78334b56428075448a2a04f90eb9d44e346af464
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-14 23:48:59 -0300

    add color scale

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const width = 1200;
+const height = 400;
+const padding = 60;
+
 fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json')
   .then(response => response.json())
   .then(data => {
@@ -18,9 +22,28 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
       .attr("id", "description")
       .text(`${minYear}-${maxYear}: base temperature ${baseTemp}℃`);
 
+    const svg = d3.select("#container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+      const colorScale = d3.scaleSequential(d3.interpolateRdBu);
+
+      // Set the domain for the color scale based on your data
+      colorScale.domain([
+        d3.max(monthlyVariance, d => baseTemp + d.variance),
+        d3.min(monthlyVariance, d => baseTemp + d.variance)
+      ]);
+
+    // Create and append the heatmap rectangles
+    svg.selectAll("rect")
+      .data(monthlyVariance)
+      .enter()
+      .append("rect")
+      .attr("x", d => (d.year - minYear) * (width / (maxYear - minYear)))
+      .attr("y", d => (d.month - 1) * (height / 12))
+      .attr("width", width / (maxYear - minYear))
+      .attr("height", height / 12)
+      .style("fill", d => colorScale(baseTemp + d.variance));
+
   });
-
-
-
-
-
